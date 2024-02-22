9e333c03d485b756ceee49c46ce9908d415b06a6
Monica Tomlinson <mcdmtomlinson@outlook.com>
2024-02-22 04:44:00 -0700

    implemented tail activity from Compass as part of lotide project

---
+++ tail.js
@@ -0,0 +1,15 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+  }
+};
+
+const tail = (arr) => {
+  return arr.slice(1);
+};
+
+const words = ['Yo Yo', 'Lighthouse', 'Labs'];
+tail(words);
+assertEqual(words.length, 3);
