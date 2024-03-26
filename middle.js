52bf5b93a6f1d1363524a8153f60ba555cebeaf8
Monica Tomlinson <mcdmtomlinson@outlook.com>
2024-03-26 17:46:21 -0600

    Refactored as per M01W04 lotide instructions

--- middle.js
+++ middle.js
@@ -1,26 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  //make sure length is the same
-  if (arr1.length === arr2.length) {
-    //for loop to make sure each element matches in the right spot
-    for (let i = 0; i < arr1.length; i ++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-  } else {
-    return false;
-  }
-  //if none of the false cases were satisfied, then equality is maintained between arr1 and arr2
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log('The arrays are equal');
-  } else {
-    console.log('The arrays are not equal');
-  }
-};
+const eqArrays = require('./eqArrays');
 
 /*Implement middle which will take in an array and return an array of
 the middle-most element(s) of the given array.
@@ -49,15 +27,6 @@ const middle = function(arr) {
     middleArray.push(arr[Math.ceil(arr.length/2) - 1])
     return middleArray;
   }
-}
-let oneElement = middle([1]);
-let twoElements = middle([1,2]);
-let threeElements = middle([1,2,3]);
-let oddArray = middle([1,2,3,4,5,]);
-let evenArray = middle([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]);
+};
 
-assertArraysEqual([], oneElement);
-assertArraysEqual([], twoElements);
-assertArraysEqual([2], threeElements);
-assertArraysEqual([3], oddArray);
-assertArraysEqual([8,9], evenArray);
+module.exports = middle
