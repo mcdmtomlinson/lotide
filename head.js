1db5f01b8cefaa7337bf3583a4564b4e13a97bf1
Monica Tomlinson <mcdmtomlinson@outlook.com>
2024-03-26 17:18:21 -0600

    refactored code as per M01W04 instructions

--- head.js
+++ head.js
@@ -1,10 +1,4 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = (arr) => {
   if (arr.length === 0) {
@@ -13,6 +7,4 @@ const head = (arr) => {
   return arr[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1, 2, 3, 4]), 1);
+module.exports = head;
