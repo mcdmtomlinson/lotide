e96adaa2245f742b8457dc508e1345827bda40d1
Monica Tomlinson <mcdmtomlinson@outlook.com>
2024-03-27 13:29:27 -0600

    incorporated mocha and chai testing

--- without.js
+++ without.js
@@ -1,26 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  //make sure length is the same
-  if (arr1.length === arr2.length) {
-    //for loop to make sure each element matches in the right spot
-    for (let i = 0; i < arr1.length; i ++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-  } else {
-    return false;
-  }
-  //if none of the false cases were satisfied, then equality is maintained between arr1 and arr2
-  return true;
-}
 
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log('The arrays are equal');
-  } else {
-    console.log('The arrays are not equal');
-  }
-}
 
 /*
 Implement without which will return a subset of a given array, removing unwanted elements.
@@ -30,27 +8,10 @@ Source array should not be modified
 */
 
 //Find the unwanted elements that are present in the source array
-const without = function(sourceArr, itemsToRemove) {
-  let wantedItems = [];
-  for (item of sourceArr) {
-    if (!itemsToRemove.includes(item)) {
-      wantedItems.push(item)
-    }
-  }
-  return wantedItems;
-}
+const without = function(arr,withoutElems) {
+  return arr.filter(ele => !withoutElems.includes(ele));
+};
+
+module.exports = without;
+
 //
-let source = [1,2,3];
-let dontWant = [1, '2'];
-let expected = [2, 3]
-console.log(assertArraysEqual(expected,without(source,dontWant)));
-console.log(assertArraysEqual(source, [1,2,3]));
-source = ['1', '2'];
-dontWant = [1,2];
-expected = ['1', '2'];
-console.log(assertArraysEqual(expected,without(source,dontWant)));
-console.log
-source = ['hello', 2];
-dontWant = ['hello'];
-expected = [2]
-console.log(assertArraysEqual(expected,without(source,dontWant)));
